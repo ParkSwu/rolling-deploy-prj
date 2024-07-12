38c45e4608e22309b77fabb184aaba84eda55358
ParkSwu <138212965+ParkSwu@users.noreply.github.com>
2024-07-12 13:29:35 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "<h1 style='color:red;'>web hook test!!!</h1>";
+        return "<h1 style='color:red;'>back test!!!</h1>";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
